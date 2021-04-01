7aecf1844ecf4bc6ef6f1505bdedde7fced114fe
Iguan <o_da2000@list.ru>
2021-04-01 21:51:22 +0300

    сделано  в виде компонента vue

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const val = "https://www.cbr-xml-daily.ru/daily_json.js";
-const vm = new Vue({
-    el: '#app',
-    data: {
-        results: []
+Vue.component('exchange', {
+    template: `  
+    <div class="exchange">
+        <h3 class="text-center">Курсы валют</h3>
+        <div class="columns medium-4" v-for="result in results.Valute">
+            <div class="card">
+                <div class="card-divider">
+                    <p>{{result.Name}}</p>
+                    <p>{{result.CharCode}}</p>
+                    <p>{{'Сегодня: ' + result.Value}}</p>
+                    <p>{{'Вчера: ' + result.Previous}}</p>
+                </div>
+            </div>
+        </div>
+    </div>`,
+
+    data() {
+        return {
+            results: []
+        }
     },
-    mounted() {
+
+    created() {
         axios.get(val).then(response => {
             this.results = response.data
         })
-    },
+    }
+
+})
+
+const val = "https://www.cbr-xml-daily.ru/daily_json.js";
+
+app = new Vue({
+    el: '#app',
 });
